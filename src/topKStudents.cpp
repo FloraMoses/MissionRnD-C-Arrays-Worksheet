ea06a9cb9bc45ca8ef304f1133196d603383635d
FloraMoses <flora2548@gmail.com>
2015-12-08 00:07:20 +0530

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -17,15 +17,33 @@ NOTES:
 
 #include <iostream>
 #include <malloc.h>
-
+struct student* sort1(struct student*, int);
 struct student {
 	char *name;
 	int score;
 };
-
 struct student ** topKStudents(struct student *students, int len, int K) {
-	if (K <= 0) 
+	if (K <= 0)
 		return NULL;
-	struct student **result = NULL;
+	if (K > len)
+		K = len;
+	students = sort1(students, len);
+	struct student **result = (struct student**)malloc(sizeof(struct student) * K);
+	for (int i = len - K, j = 0; i < len; i++, j++) {
+		result[j] = &students[i];
+	}
 	return result;
+}
+struct student* sort1(struct student *students, int len) {
+	struct student temp;
+	for (int i = 0; i < len - 1; ++i) {
+		for (int j = 0; j < len - i - 1; ++j) {
+			if (students[j].score > students[j + 1].score) {
+				temp = students[j + 1];
+				students[j + 1] = students[j];
+				students[j] = temp;
+			}
+		}
+	}
+	return students;
 }
